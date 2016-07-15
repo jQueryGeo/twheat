5b5bf1b9c996d16aa2343a5505d24501ed87c9c4
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2016-07-15 01:09:09 -0400

    #12 - jspm package and main

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 System.config({
-  baseURL: "/",
+  baseURL: "/proj/jQueryGeo/twheat/",
   defaultJSExtensions: true,
   transpiler: "babel",
   babelOptions: {
@@ -18,7 +18,7 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
     "jquery": "npm:jquery@2.2.4",
-    "jquery.geo": "npm:jquery.geo@1.0.0-rc1.1",
+    "jquery.geo": "npm:jquery.geo@1.0.0-rc1.2.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
@@ -29,10 +29,13 @@ System.config({
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.3"
+      "process": "npm:process@0.11.5"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
+    },
+    "github:jspm/nodelibs-vm@0.1.0": {
+      "vm-browserify": "npm:vm-browserify@0.0.4"
     },
     "npm:assert@1.4.1": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
@@ -60,18 +63,23 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:jquery.geo@1.0.0-rc1.1": {
+    "npm:jquery.geo@1.0.0-rc1.2.1": {
       "jquery": "npm:jquery@2.2.4"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:process@0.11.3": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    "npm:process@0.11.5": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "vm": "github:jspm/nodelibs-vm@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:vm-browserify@0.0.4": {
+      "indexof": "npm:indexof@0.0.1"
     }
   }
 });
